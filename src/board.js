eb68d0e593b42c9f0d958007946042228fdddb1a
2020-DEV-001 <57988639+2020-DEV-001@users.noreply.github.com>
2019-11-20 18:43:32 +0530

    File updated to display the status value

--- src/board.js
+++ src/board.js
@@ -3,30 +3,32 @@ import './index.css';
 import Square from './square';
 
 export default class Board extends React.Component {
-    addSquare() {
-        return <Square />;
+    addSquare(i) {
+        //code to pass a prop called value to the Square
+        return <Square value={i}/>;
     }
 
     render() {
+        const status = 'Player: X';
         return (
-            <div>
-                <div className="status">
-                ???
+            <div id="game-info">
+                <div className="status" >
+                {status}
                 </div>
                 <div className="board-row">
-                    {this.addSquare()}
-                    {this.addSquare()}
-                    {this.addSquare()}
+                    {this.addSquare(0)}
+                    {this.addSquare(1)}
+                    {this.addSquare(2)}
                 </div>
                 <div className="board-row">
-                    {this.addSquare()}
-                    {this.addSquare()}
-                    {this.addSquare()}
+                    {this.addSquare(3)}
+                    {this.addSquare(4)}
+                    {this.addSquare(5)}
                 </div>
                 <div className="board-row">
-                    {this.addSquare()}
-                    {this.addSquare()}
-                    {this.addSquare()}
+                    {this.addSquare(6)}
+                    {this.addSquare(7)}
+                    {this.addSquare(8)}
                 </div>
             </div>
         );
