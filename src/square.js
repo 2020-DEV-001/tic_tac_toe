1cee794e281d0f1e64fc87b8e3bd178de6f87073
2020-DEV-001 <57988639+2020-DEV-001@users.noreply.github.com>
2019-11-20 20:07:20 +0530

    Updated the square.js render method

--- src/square.js
+++ src/square.js
@@ -2,11 +2,19 @@ import React from 'react';
 import './index.css';
 
 export default class Square extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          value: null,
+        };
+      }
+
     render() {
         return (
             //Change Squares render method to show that value 
-            <button className="square">
-                {this.props.value}
+            <button type="button" className="square" onClick={this.handleClick}>
+                {this.state.value}
             </button>
         );
     }
