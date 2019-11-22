c7f6a4267924716aa4e020ec4ecab078958a8710
2020-DEV-001 <57988639+2020-DEV-001@users.noreply.github.com>
2019-11-22 21:40:40 +0530

    file modified for updating child component

--- src/square.js
+++ src/square.js
@@ -14,8 +14,13 @@ export default class Square extends React.Component {
     render() {
         return (
             //Code modified to check if the square is clicked
-            <button className="square" onClick={function() { alert('Square is clicked'); }}>
-            {this.props.value}
+            //<button className="square" onClick={function() { alert('Square is clicked'); }}>
+            //Code modified for updating child component
+            <button
+            className="square"
+            onClick={() => this.setState({value: 'X'})}
+          >
+            {this.state.value}
           </button>);
     }
 }
