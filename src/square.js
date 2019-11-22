0dace95cacf07041e48baecb0b2c671894d553e8
2020-DEV-001 <57988639+2020-DEV-001@users.noreply.github.com>
2019-11-23 00:07:39 +0530

    file modofed for click logic

--- src/square.js
+++ src/square.js
@@ -16,11 +16,16 @@ export default class Square extends React.Component {
             //Code modified to check if the square is clicked
             //<button className="square" onClick={function() { alert('Square is clicked'); }}>
             //Code modified for updating child component
-            <button
-            className="square"
-            onClick={() => this.setState({value: 'X'})}
-          >
-            {this.state.value}
-          </button>);
+          //   <button
+          //   className="square"
+          //   onClick={() => this.setState({value: 'X'})}
+          // >
+          //   {this.state.value}
+          // </button>);
+          <button
+          className="square"
+          onClick={() => this.props.onClick()}>
+                  {this.props.value}
+        </button>);
     }
 }
