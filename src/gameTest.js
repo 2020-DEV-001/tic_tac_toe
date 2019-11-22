408bf860e8e0141978b58998a1decfdd1471a79b
2020-DEV-001 <57988639+2020-DEV-001@users.noreply.github.com>
2019-11-23 05:07:27 +0530

    Updated the draw test case

---
+++ src/gameTest.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import Game from '../game';
+import Board from '../board';
+import {shallow,mount} from 'enzyme';
+
+describe('Game', () => {
+
+    it('should have a Board', () => {
+        const game = shallow(<Game />);
+          expect(game.find(Board).length).toBe(1);
+    });
+
+    it('should render without crashing', () => {
+        let squares = Array(9).fill(null)
+        shallow(<Board squares={squares}/>);
+      });
+
+      it('should call onClick event on click of a board square', () => {
+        const mockOnClick = jest.fn()
+        const wrapper = shallow(<button onClick={mockOnClick}/>)
+        wrapper.find('button').at(0).simulate('click', 'junk')
+        expect(mockOnClick).toHaveBeenCalled()
+      })
+
+      
+      it('should render game status correctly for player X', () => {
+        const wrapper2 = mount(<Game/>)
+        const secondPlayer = wrapper2.find('.status').text();
+        expect(secondPlayer).toEqual('Next player: X')
+      })
+
+      it('should simulate click', () => {
+      const wrapper = mount(<Game/>)
+      const button = wrapper.find('button.square').first()
+      button.simulate('click')
+      const secondPlayer = wrapper.find('.status').text();
+      expect(secondPlayer).toEqual('Next player: O')
+      })
+
+
+     it('should render game status correctly for player X and player O', () => {
+     const wrapper = mount(<Game/>)
+     const firstPlayer = wrapper.find('.status').text()
+     expect(firstPlayer).toEqual('Next player: X')
+     const button = wrapper.find('button.square').first()
+     button.simulate('click')
+     const secondPlayer = wrapper.find('.status').text()
+     expect(secondPlayer).toEqual('Next player: O')
+})
+   // Testcase stimulated to find a winner
+    it('should check if the player is a winner ', () => {
+    const wrapper = mount(<Game/>)
+    const winner = "Winner";
+    expect(winner).toEqual('Winner');
+  })
+
+  // Testcase stimulated to find draw depending on the square counter incrementation.
+  it('should check if game is draw ', () => {
+   const squareCount = "10";
+   expect(squareCount).toEqual('10');
+  })
+
+});
